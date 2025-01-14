4b83fd663a4e1d6f49a4980fe85f3774b805607b
Nielspsj <nielspsj@hotmail.com>
2025-01-14 22:00:26 +0100

    Align with ground
    1. The mount now aligns its rotation to the ground it walks on.
    2. You can adjust the layer and rotation smoothness.
    3. It is separate from the actual movement.

---
+++ Assets/Scripts/AlignWithGround.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithGround : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    public float rotationSmoothing;
+
+    private CharacterController body;
+    private RaycastHit groundSurfaceHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayToGround();
+        RotateToAlign();
+    }
+
+    private void RayToGround()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 5, groundLayer))
+        {
+            Debug.Log("Hit the ground!");
+            groundSurfaceHit = hit;
+        }
+    }
+    private void RotateToAlign()
+    {
+        Quaternion newRotation = Quaternion.FromToRotation(transform.up, groundSurfaceHit.normal) * transform.rotation;
+        body.transform.rotation = Quaternion.Slerp(body.transform.rotation, newRotation, Time.deltaTime * rotationSmoothing);
+        Debug.Log("Rotating");
+    }
+}
